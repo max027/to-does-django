4cdc80c03b087cbcd08aa3872231dca350e427e4
max027 <saurabhwajage887@gmail.com>
2021-10-07 10:27:36 +0530

    initial commit

---
+++ base/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import TaskDetail, Tasklist,TaskCreate,TaskUpdate,DeleteView
+urlpatterns = [
+    path('',Tasklist.as_view(),name='tasks'),
+    path('task/<int:pk>/',TaskDetail.as_view(),name='task'),
+    path('task-create',TaskCreate.as_view(),name='task-create'),
+    path('task-update/<int:pk>/',TaskUpdate.as_view(),name='task-update'),
+    path('task-delete/<int:pk>',DeleteView.as_view(),name='task-delete'),
+]
